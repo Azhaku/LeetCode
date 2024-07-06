38de79239fc98a8b0bd23e096dc65cb8cf9c359e
Azhaku <azhakudurai476@gmail.com>
2024-07-06 17:35:26 +0530

    new commit

--- Top Interview 150/Matrix/48. Rotate Image.py
+++ Top Interview 150/Matrix/48. Rotate Image.py
@@ -24,16 +24,36 @@ class Solution(object):
     def rotate(self, matrix):
         length = len(matrix[0])
         max = length-1
-        start = 0
-
-            
+        start = 0  
         for i in range(0,length-1):
             for j in range(i,(length - (i+1))):
-
+                
+                # save the top left value
                 temp = matrix[start+i][max - j]
+                
+                # update the top left with bottom left
                 matrix[start+i][max - j] = matrix[j+start][start+i]
+                
+                # update the bottom left with bottom right
                 matrix[j+start][start+i] = matrix[max - i][j]
+                
+                # update the bottom right with top right
                 matrix[max - i][j] = matrix[max-j][max - i]
+
+                # update the top right with top left value
                 matrix[max-j][max - i] = temp
 
         return matrix
+    
+    
+# method - 2
+def rotate(self, matrix: List[List[int]]) -> None:
+    # First, reverse the matrix rows (flip the matrix vertically)
+    matrix.reverse()
+    
+    # Now, transpose the matrix (swap rows with columns)
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i+1, n):  # Note: Starting from `i+1` to avoid redundant swaps
+            # Swap matrix[i][j] and matrix[j][i]
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
