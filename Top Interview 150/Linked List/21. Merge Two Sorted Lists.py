76f9115c52db95a6552d7f3ad614b28bea943a2c
Azhaku <azhakudurai476@gmail.com>
2024-07-09 21:31:47 +0530

    new commit

--- Top Interview 150/Linked List/21. Merge Two Sorted Lists.py
+++ Top Interview 150/Linked List/21. Merge Two Sorted Lists.py
@@ -26,13 +26,7 @@ class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
         """
